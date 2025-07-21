f2a55af3bab8f5689d909c354491142c36d7408f
Deepak Kumar Majhi <155293118+deepak443mki@users.noreply.github.com>
2025-07-21 04:32:27 +0000

    Lecture4DSA

---
+++ lect4.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    // square pattern
+    //  int n = 4;
+    //  int num  = 1;
+    //  char ch = 'A';
+    //  for (int i = 1; i <= n; i++)
+    //  {
+    //      for (int j = 1; j <= n; j++)
+    //      {
+    //          cout <<num << " ";
+    //          num++;
+    //      }
+    //      cout << "\n";
+    //  }
+
+    // int n = 4;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // char ch = 'A';
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << ch << " ";
+    //     }
+    //     ch++;
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = i; j >= 1; j--)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // int num = 1;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // char ch = 'A';
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << "\n";
+    // }
+
+    // for (char i = 'A'; i <= 'D'; i++)
+    // {
+    //     for (char j = i; j >= 'A'; j--)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int k = 1; k < i; k++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int j = 1; j <= n - i + 1; j++)
+    //     {
+    //         cout << i << " ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // char ch = 'A';
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int k = 1; k < i; k++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int j = 1; j <= n - i + 1; j++)
+    //     {
+    //         cout << ch << " ";
+    //     }
+    //     ch++;
+    //     cout << "\n";
+    // }
+
+    // int n = 4;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n-i; j++)
+    //     {
+    //         cout <<" ";
+    //     }
+    //     for (int k = 1; k <= i ; k++)
+    //     {
+    //         cout << k << "";
+    //     }
+    //     for (int l = i; l > 1; l--)
+    //     {
+    //         cout << l-1 << "";
+    //     }
+
+    //     cout << "\n";
+    // }
+
+    // Diamond pattern
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = n - i - 1; j > 0; j--)
+    //     {
+    //         cout << "   ";
+    //     }
+    //     cout << "*  ";
+    //     if (i != 0)
+    //     {
+    //         for (int k = 0; k < 2 * i - 1; k++)
+    //         {
+    //             cout << "   ";
+    //         }
+    //         cout << "*  ";
+    //     }
+    //     cout << "\n";
+    // }
+    // for (int i = 0; i <= n - 2; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << "   ";
+    //     }
+    //     cout << "*  ";
+
+    //     if (i != n - 2)
+    //     {
+    //         for (int k = 0; k < 2 * (n - i) - 5; k++)
+    //         {
+    //             cout << "   ";
+    //         }
+    //         cout << "*  ";
+    //     }
+    //     cout << "\n";
+    // }
+
+    // butterfly pattern
+    //  int n = 4;
+    //  for (int i = 1; i <= 4; i++)
+    //  {
+    //      for (int j = 1; j <= i; j++)
+    //      {
+    //          cout <<"* ";
+    //      }
+    //      for (int s = n-1; s >=i; s--)
+    //      {
+    //          cout << "  ";
+    //      }
+    //      for (int s = n-1; s >=i; s--)
+    //      {
+    //          cout << "  ";
+    //      }
+    //      for (int j = 1; j <= i; j++)
+    //      {
+    //          cout <<"* ";
+    //      }
+
+    //     cout <<"\n";
+    // }
+
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = n; j >=i; j--)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     for (int s = 1; s < i; s++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int s = 1; s < i; s++)
+    //     {
+    //         cout << "  ";
+    //     }
+    //     for (int j = n; j >=i; j--)
+    //     {
+    //         cout << "* ";
+    //     }
+
+    //     cout << "\n";
+
+    // }
+
+    return 0;
+}
